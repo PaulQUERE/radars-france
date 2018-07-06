97061749bc0326d98cce9572465cfcb59d4cfe07
Antoine Augusti <antoine.augusti@gmail.com>
2018-07-06 18:07:18 +0200

    Add files

---
+++ clean.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import json
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+BASE_PATH = 'data'
+
+records = []
+for file in [f for f in listdir(BASE_PATH) if isfile(join(BASE_PATH, f)) and f.endswith('json')]:
+    record = json.load(open(join(BASE_PATH, file), 'r'))
+    cols = [
+        'itineraireEntree', 'itineraireSortie', 'radarTronconKm',
+        'traceItineraire', 'radarGeolocalisation', 'radarDirection',
+        'radarPlace'
+    ]
+    for key in cols:
+        if record[key] == []:
+            record[key] = None
+
+    for key in ['radarEquipment', 'radarRoad']:
+        if record[key] == '-':
+            record[key] = None
+
+    if len(record['radarType']) != 1:
+        raise NotImplementedError
+    record['radarNameDetails'] = record['radarType'][0]['radarNameDetails']
+
+    # Parse vitesse
+    rules = [e['name'] for e in record['rulesMesured']]
+
+    vitesse_vl = [r for r in rules if r.startswith('Vitesse VL')]
+    record['vitesse_vehicules_legers_kmh'] = None
+    if len(vitesse_vl) == 1:
+        record['vitesse_vehicules_legers_kmh'] = int(vitesse_vl[0].split(' ')[2].strip())
+
+    vitesse_pl = [r for r in rules if r.startswith('Vitesse PL')]
+    record['vitesse_poids_lourds_kmh'] = None
+    if len(vitesse_pl) == 1:
+        record['vitesse_poids_lourds_kmh'] = int(vitesse_pl[0].split(' ')[2].strip())
+
+    # Parse coordinates
+    record['latitude'] = None
+    record['longitude'] = None
+    if record['traceItineraire'] is not None:
+        record['latitude'] = record['traceItineraire']['lat']
+        record['longitude'] = record['traceItineraire']['lon']
+    if record['radarGeolocalisation'] is not None:
+        record['latitude'] = record['radarGeolocalisation']['lat']
+        record['longitude'] = record['radarGeolocalisation']['lon']
+
+    # Parse radarTronconKm
+    if record['radarTronconKm'] is not None:
+        record['radarTronconKm'] = float(record['radarTronconKm'].replace(',', '.'))
+
+    # Parse department
+    record['department'] = record['department'].split('-')[0].strip()
+
+    records.append(record)
+
+df = pd.DataFrame(records)
+df['radarInstallDate'] = pd.to_datetime(df['radarInstallDate'], format='%d.%m.%Y')
+for col in ['created', 'changed']:
+    df[col] = pd.to_datetime(df[col], unit='s')
+df.drop(columns=[
+    'langcode', 'type', 'defaultLangcode', 'path', 'radarType',
+    'rulesMesured', 'revisionTimestamp', 'radarGeolocalisation',
+    'promote', 'revisionLog', 'revisionTranslationAffected',
+    'status', 'sticky', 'title', 'uid', 'uuid', 'revisionUid',
+    'vid',
+    'traceItineraire', 'itineraireEntree', 'itineraireSortie'
+], inplace=True)
+
+df.rename(
+    index=str,
+    columns={
+        "changed": "date_heure_dernier_changement",
+        "created": "date_heure_creation",
+        "department": "departement",
+        "nid": "id",
+        "radarDirection": "direction",
+        "radarEquipment": "equipement",
+        "radarInstallDate": "date_installation",
+        "radarNameDetails": "type",
+        "radarPlace": "emplacement",
+        "radarRoad": "route",
+        "radarTronconKm": "longueur_troncon_km",
+    },
+    inplace=True
+)
+
+df.to_csv(
+    'data/radars.csv',
+    index=False,
+    encoding='utf-8',
+    float_format='%.12g',
+    date_format='%Y-%m-%dT%H:%M:%SZ'
+)
---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+
+from requests.adapters import HTTPAdapter
+
+s = requests.Session()
+s.mount('http://stackoverflow.com', HTTPAdapter(max_retries=5))
+
+
+def endpoint(part):
+    return 'https://{base}/{part}?_format=json'.format(
+        base='radars.securite-routiere.gouv.fr',
+        part=part
+    )
+
+
+def do_request(url):
+    r = requests.get(
+        url,
+        verify=False,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+        }
+    )
+    r.raise_for_status()
+
+    return r
+
+radars = do_request(endpoint('all'))
+radars.raise_for_status()
+
+for radar in radars.json():
+    id = radar['id']
+
+    r = do_request(endpoint(id))
+    r.raise_for_status()
+
+    path = 'data/{id}.json'.format(id=id)
+    with open(path, 'w') as f:
+        f.write(json.dumps(r.json()))
